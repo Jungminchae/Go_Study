f93597d0f2786903d6327706f5e824a91d5da903
Jungminchae <minchae3618@gmail.com>
2021-03-01 20:56:41 +0900

    finish go routine

--- theory/goroutine.go
+++ theory/goroutine.go
@@ -7,14 +7,18 @@ import (
 
 // goroutine은 메인 함수가 실행되고 있을 때만 작동 됨
 func main() {
-	go sexyCount("minchae")
-	sexyCount("fuck!")
-	time.Sleep(time.Second * 5)
+	c := make(chan bool)
+	people := [5]string{"minchae", "minchae2", "minchae3", "minchae4", "minchae5"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is_sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- person + "is sexy"
 }
