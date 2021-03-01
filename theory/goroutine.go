64cca79f545bb55678fd86a3374079f3dc239340
Jungminchae <minchae3618@gmail.com>
2021-03-01 16:01:15 +0900

    add goroutine

---
+++ theory/goroutine.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// goroutine은 메인 함수가 실행되고 있을 때만 작동 됨
+func main() {
+	go sexyCount("minchae")
+	sexyCount("fuck!")
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is_sexy", i)
+		time.Sleep(time.Second)
+	}
+}
